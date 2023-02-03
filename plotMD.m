3243d3fa736bc88adfe8a81c05552d25009631fa
hamreliusfilip <filha243@student.liu.se>
2023-02-03 16:32:00 +0100

    Thrust plot
    + Thrust plot
    + Fullscreen viewing

--- plotMD.m
+++ plotMD.m
@@ -1,56 +1,76 @@
 function [] = plotMD(y,t)
 
-x = zeros(1,561);
+    % Mass
+    x = zeros(1,561);
 
-for i=1:length(x)
-    x(1,i) = mass(massFuel(t(i)));
-end
+    for i=1:length(x)
+        x(1,i) = mass(massFuel(t(i)));
+    end
 
-q = zeros(1,561);
+    % Air resistance
+    q = zeros(1,561);
 
-for i=1:length(q)
-    q(1,i) = AirPlotHelpFunc(y(i,:));
-end
+    for i=1:length(q)
+        q(1,i) = AirPlotHelpFunc(y(i,:));
+    end
 
-set(gcf,'Visible','on')
+    % Thrust
+     u = zeros(1,561);
 
-subplot(3,2,1);
-ani1=animatedline('Color','r','LineWidth',2);
+    for i=1:length(q)
+        u(1,i) = getThrust(y(i,3));
+    end
+
+    set(gcf,'Visible','on')
+    set(gcf, 'Position', get(0, 'Screensize'));
+
+    subplot(3,2,1);
+    ani1=animatedline('Color','r','LineWidth',2);
     xlabel("Seconds")
     ylabel("m/s")
     title("Velocity over time")
     grid on
     hold off
 
-subplot(3,2,2);
-ani2=animatedline('Color','blue','LineWidth',2);
+    subplot(3,2,2);
+    ani2=animatedline('Color','blue','LineWidth',2);
     xlabel("Seconds")
     ylabel("m/s^2")
     title("Acceleration over time")
     grid on
     hold off 
 
-subplot(3,2,3);
-ani3=animatedline('Color','black','LineWidth',2);
+    subplot(3,2,3);
+    ani3=animatedline('Color','black','LineWidth',2);
     xlabel("Seconds")
     ylabel("N")
     title("Drag over time")
     grid on
     hold off 
 
-subplot(3,2,4);
-ani4=animatedline('Color','m','LineWidth',2);
+    subplot(3,2,4);
+    ani4=animatedline('Color','m','LineWidth',2);
     xlabel("Seconds")
     ylabel("KG")
     title("Mass over time")
     grid on
     hold off 
     
-subplot(3,2,5);  
-ani5=animatedline('Color','m','LineWidth',2);
-    title("Trajectory")
+    subplot(3,2,5);
+    ani5=animatedline('Color','m','LineWidth',2);
+    xlabel("Seconds")
+    ylabel("Thrust")
+    title("Thrust change over time")
+    grid on
+    hold off 
+    
+    subplot(3,2,6);  
+    ani6=animatedline('Color','red','LineWidth',15);
+    ani7=animatedline('Color','black','LineWidth',15);
+    title("Trajectory 3D")
     grid on
     view(3);
+    zoom(1);
     hold off 
     
 for i = 1:length(y)
@@ -60,7 +80,6 @@ for i = 1:length(y)
     addpoints(ani1,t(i),y(i,2));
     addpoints(ani1,t(i),y(i,3));
     hold off 
-    
     
     addpoints(ani2,t(i),y(i,4));
     hold on
@@ -74,7 +93,11 @@ for i = 1:length(y)
     addpoints(ani4,t(i),x(i));
     hold off
     
-    addpoints(ani5,y(i,1), y(i,2), y(i,3));
+    addpoints(ani5,t(i),u(i));
+    hold off
+    
+    addpoints(ani6,y(i+2,1), y(i+2,2), y(i+2,3));
+    addpoints(ani7,y(i,1), y(i,2), y(i,3));
     hold off
     
     drawnow
