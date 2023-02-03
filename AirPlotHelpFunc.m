b2814a2590c4c75eeea7b219e062c72978e0c7cf
hamreliusfilip <filha243@student.liu.se>
2023-02-03 11:48:15 +0100

    Plot madness
    Plot: Velocity, Acc, Mass, Drag

---
+++ AirPlotHelpFunc.m
@@ -0,0 +1,13 @@
+function Fdrag = air(y)
+
+    Cd = 0.5; % Drag coefficient
+    A = 17; % Reference area of the rocket
+    
+    rho = descendingAirDensity(y(3));
+    
+    v = sqrt(y(4)^2 + y(5)^2 + y(6)^2); % Velocity
+    
+    Fdrag = 0.5 * Cd * A * rho * v^2; % Drag force on the rocket
+    
+end
+
