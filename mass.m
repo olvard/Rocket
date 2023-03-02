ef4faca6b58b20d53a38b67cad4ef18ccfae805f
hamreliusfilip <filha243@student.liu.se>
2023-03-02 11:13:54 +0100

    Fix

--- mass.m
+++ mass.m
@@ -2,13 +2,9 @@ function rocketMass = mass(fuelLeft)
 
     % Constant parameters
     payloadToLEO = 22800;
-    payloadToGTO = 8300;
-    payloadToMARS = 4020;
 
     firstStageRocketMass = 25600;
     secondStageRocketMass = 3900;
-
-    tankMass = 500;
     
     % Initialize rocket mass
     rocketMass = 0;
